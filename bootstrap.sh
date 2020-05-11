3367068b58a73bc5b7a9e1f65f98932067a33f24
Jarret Lavallee <jarret.lavallee@puppet.com>
2020-05-11 15:57:19 -0600

    A more approipate name

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ kubectl apply -f flux/namespace.yaml
 kubectl apply -f flux/flux-ssh.yaml
 helm repo add fluxcd https://charts.fluxcd.io
 # https://github.com/fluxcd/flux/issues/3011
-helm upgrade -i flux fluxcd/flux --namespace flux -f flux/flux.txt
+helm upgrade -i flux fluxcd/flux --namespace flux -f flux/flux.txt --version 1.2.0
 #  --set git.url=git@github.com:jarretlavallee/flux \
 #  --set git.secretName=flux-ssh \
 #  --set syncGarbageCollection.enabled=true \
